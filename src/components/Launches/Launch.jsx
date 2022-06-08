a8345b964c5ca3a453b33e19b4ddaefe2d075fc3
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-08 21:29:48 +0800

    Add some details for launch

--- src/components/Launches/Launch.jsx
+++ src/components/Launches/Launch.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React from 'react';
 
 const Launch = ({ launch }) => {
-  return (
-    <div className="launch row">
-      <h2>{launch.mission_name}</h2>
-      <p className="launch__details">{launch.details}</p>
-    </div>
-  );
+	return (
+		<div className='launch row launch__item'>
+			<h2>{launch.mission_name}</h2>
+			<div className='launch__details launch__meta'>
+				<span>{launch.launch_year}</span>
+				{launch.launch_success ? (
+					<span className='launch__status--success'>success</span>
+				) : (
+					<span className='launch__status--danger'>failed</span>
+				)}
+			</div>
+			<p className='launch__details'>{launch.details}</p>
+		</div>
+	);
 };
 
 export default Launch;
