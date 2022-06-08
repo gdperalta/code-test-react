8a4c5780f3809936260ba43fb6b867a3e26050c3
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-08 17:36:48 +0800

    Add spaceX launches as heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import Launches from './components/Launches/Launches';
+import React from "react";
+import "./App.css";
+import Launches from "./components/Launches/Launches";
 
 function App() {
-	return (
-		<div className="App">
-			Start coding!!!
-			<Launches />
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>SpaceX Launches</h1>
+      <Launches />
+    </div>
+  );
 }
 
 export default App;
