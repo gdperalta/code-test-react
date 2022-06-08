f79a2627abeb6b6c0c13e5993cfbb2736dd89299
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-08 21:30:28 +0800

    Removed states and moved to app.jsx Add div of end of list if fullyLoaded

--- src/components/Launches/Launches.jsx
+++ src/components/Launches/Launches.jsx
@@ -1,52 +1,28 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
-import { fetchLaunches } from '../../services/SpacexService';
 import Spinner from '../Spinner/Spinner';
 import Launch from './Launch';
 
-const Launches = () => {
-	const [launches, setlaunches] = useState([]);
-	const [offset, setOffset] = useState(0);
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		setIsLoading(true);
-
-		getLaunches();
-	}, [offset]);
-
-	const getLaunches = async () => {
-		const data = await fetchLaunches(offset);
-		console.log(offset);
-		setlaunches([...launches, data].flat());
-		setIsLoading(false);
-	};
-
-	const handleScroll = (e) => {
-		const bottom =
-			e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
-		if (bottom) {
-			setOffset(offset + 10);
-		}
-	};
-
+const Launches = ({ launches, isLoading, fullyLoaded }) => {
 	return (
-		<div className='main__wrapper' onScroll={handleScroll}>
-			{isLoading && launches.length === 0 ? (
-				<Spinner />
-			) : (
-				launches.map((launch) => {
-					return (
-						<Launch
-							key={`${launch.flight_number}-${launch.launch_date_unix}`}
-							launch={launch}
-						/>
-					);
-				})
-			)}
+		<div>
+			{launches.map((launch) => {
+				return (
+					<Launch
+						key={`${launch.flight_number}-${launch.launch_date_unix}`}
+						launch={launch}
+					/>
+				);
+			})}
 			{isLoading && launches.length > 0 ? (
 				<div className='spinner-container'>
 					<Spinner />
+				</div>
+			) : (
+				''
+			)}
+			{fullyLoaded ? (
+				<div className='max-reached'>
+					<p>End of list</p>
 				</div>
 			) : (
 				''
