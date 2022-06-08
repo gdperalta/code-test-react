aa9063ef31ab3c1288b2ce2a2f214659b202979a
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-08 21:28:23 +0800

    Add query for launch_year

--- src/services/SpacexService.js
+++ src/services/SpacexService.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.spacexdata.com/v3';
 
-export const fetchLaunches = async (offset = 0) => {
-	const response = await axios.get(`/launches?limit=10&offset=${offset}`);
-
+export const fetchLaunches = async (offset, query) => {
+	console.log('hello');
+	const response = await axios.get(
+		`/launches?limit=10&offset=${offset}&launch_year=${query}`
+	);
 	return await response.data;
 };
