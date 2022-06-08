5643b18ec5d3624406b5872495ef34cece36ec1c
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-06-08 15:20:27 +0800

    Add component Launches

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import Launches from './components/Launches/Launches';
 
 function App() {
-  return <div className="App">Start coding!!!</div>;
+	return (
+		<div className="App">
+			Start coding!!!
+			<Launches />
+		</div>
+	);
 }
 
 export default App;
